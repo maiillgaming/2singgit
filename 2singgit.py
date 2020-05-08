055eec7282b8789d92be7a3b8c6893560c89fc9a
maiillgaming <65038091+maiillgaming@users.noreply.github.com>
2020-05-08 17:58:58 +0000

    Add files via upload

---
+++ 2singgit.py
@@ -0,0 +1,39 @@
+#!/bin/usr/python
+
+import random
+import sys
+import time
+import os
+os.system('clear')
+def singgit(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan singgit
+        time.sleep(random.random() * 0.2)
+singgit('\33[31;1mHALO KONTOL MASIH MAIN TERMUX\nHAH BANGSAT?? LO ASU APA LO, IRI BILANG BOSS')
+singgit('\33[35;1mSUMPAH GUE GAK NIAT BIKIN TOOLS HACK HACKAN TAI\nANJING\nAWAS LO KALO MASUK TOOLS INI GW BACOK ENTEE!!')
+singgit('\33[36;1mYAUDAH BRO PASSWORD CHAT FACEBOOK GUEE OKK Fb: Maiill Gaming')
+os.system('clear')
+
+print ("\33[32;1mLOGIN DULU BOSS BIAR GAK CROT")
+user = raw_input("\33[33;1mUsername: ")
+import getpass
+sandi = getpass.getpass()
+if sandi == '2' and user == 'singgit':
+     print ("\33[32;1mLOGIN SUCCES ASU!")
+else:
+     print ("\33[31;1mAWOKAWOK LOGIN GAGAL, MAKANYA CHAT FB GUE TOLOL!!!")
+os.system('clear')
+
+import random
+import sys
+import time
+def singgit(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan singgit
+        time.sleep(random.random() * 0.1)
+singgit('\33[36;1mSILAHKAN CARI USERNAME DAN PASSWORDNYA\nDI FACEBOOK REAL MAIILL GAMING JIKA INGIN MEMAKAI\nTOOLS INI ANDA HARUS USAHA AGAR HASILNYA MENYENANGKAN\n          \33[33;1mTERIMAKASIH SALAM\n           \33[34;1mMAIILL\33[31;1mGAMING')
+
